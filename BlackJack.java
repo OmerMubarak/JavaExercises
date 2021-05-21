9f0f63e77b9260c71e9d385cdb404a01724b167b
OmerMubarak <omer.khalafalla@outlook.com>
2021-05-21 15:58:24 +0100

    OK: End of lessons on Friday from Pawel! Uploaded all of my completed java exercise still a few to go.

--- BlackJack.java
+++ BlackJack.java
@@ -3,66 +3,28 @@ package com.qa.qacommunity;
 
 public class BlackJack {
 
-	public static int A;
-	public static int B;
-
 	public static void main(String[] args) {
-		System.out.println(blackJackNum(15, 18));
+		System.out.println(blackJackNum(22, 21));
 
 	}
 
-	public static int blackJackNum(int A, int B) {
-		if (A > B && A <= 21) {
-			return A;
-			}
-			if (A > B && A > 21 && B <= 21) {
-				return B;
-			} else {
-				return 0;
-			}
-
-		 if (B > A && B <= 21){
-			return B;
-			}
-			if (B > A && B > 21 && A <= 21){
-				return A;
-			}
-			else{
-	return 0;			
-			}
-			}
-		
-}}
-
-//play(A, B)
-
-//if A > B && A =< 21, print A
-//if A > B && A > 21 && B =< 21, print B
-//if B > A && B =< 21, print B
-//if B > A && B > 21 && A =< 21, print A
-//if B > 21 && A > 21, print 0
-
-public static int blackjack(int A, int B){
-	if (A > B) {
-		if (A <= 21) {
-			System.out.println(A);
-			if (A > 21) {
-				if (B <= 21)
-				System.out.println(B);
+	public static int blackJackNum(int player1, int player2) {
+		if (player1 > 21 && player2 > 21) {
+			return 0;
+		} else {
+			if (player1 > player2) {
+				if (player1 > 21) {
+					return player2;
 				} else {
-					System.out.println(0);
+					return player1;
 				}
-			}
-	} else {
-		if (B > A) {
-			if (B <= 21) {
-				System.out.println(B);
-				if (B > 21) {
-					if (A <= 21)
-					System.out.println(A);
-					} else {
-						System.out.println(0);
-					}
+
+			} else {
+				if (player2 > 21) {
+					return player1;
+				} else {
+					return player2;
+				}
 			}
 		}
 	}
